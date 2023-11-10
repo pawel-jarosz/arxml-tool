32fb1e14fd28d1aa74d413a413ab10fd31f9a2ba
Paweł Jarosz <jarosz.pawel@outlook.com>
2023-11-10 16:57:52 +0100

    Add tinyxml2 dependency

--- library/arxml_parser.cpp
+++ library/arxml_parser.cpp
@@ -4,13 +4,21 @@
 
 #include <arxml/arxml_parser.hpp>
 
+#include <tinyxml2.h>
+
 namespace arxml {
+
+    static void traverse_xml(tinyxml2::XMLElement* element) {
+
+    }
 
     void ArxmlModelBuilder::addModelUnitFromSource(const std::string& unit_name, io::IModelSource& source) {
         if (not m_root) {
             m_root = std::move(m_element_factory.createRoot());
         }
         auto model_unit = m_element_factory.createModelUnit(std::string(unit_name));
+        tinyxml2::XMLDocument xml;
+        xml.Parse(source.getContent().c_str());
         m_root->registerModelUnit(unit_name, std::move(model_unit));
     }
 
