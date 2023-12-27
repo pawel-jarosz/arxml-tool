f3f74cfbce3b7f2385b0680c2cb4ca9bf6fe1d16
Paweł Jarosz <jarosz.pawel@outlook.com>
2023-12-27 13:04:45 +0100

    Add model project structure

---
+++ include/arxml/project.hpp
@@ -0,0 +1,23 @@
+//
+// Created by Paweł Jarosz on 27.12.2023.
+//
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace arxml::project {
+
+    class ModelProject {
+    public:
+        ModelProject() = default;
+
+        bool addFile(const std::string& file);
+        bool addDirectory(const std::string& directory);
+        const std::vector<std::string>& getFileList() const { return m_files; }
+    private:
+        std::vector<std::string> m_files;
+    };
+
+}
