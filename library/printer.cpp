9875301dde999a8a112c6af9e776a55f2e1a1e55
Paweł Jarosz <jarosz.pawel@outlook.com>
2024-01-03 16:33:04 +0100

    Draft of the dumping arxml file

--- library/printer.cpp
+++ library/printer.cpp
@@ -151,18 +151,22 @@ namespace arxml::printer {
         }
     }
 
+    template<class AutosarModelObject>
+    void generic_print(dfs::TraversalCallback& callback, AutosarModelObject& object) {
+        dfs::traverse_model(object, callback);
+    }
+
     TreePrinter::TreePrinter(std::ostream& stream, int indent)
     : m_callback{new TreePrinterCallback{stream, indent}}
     {
 
     }
 
-    template<class AutosarModelObject>
-    void generic_print(dfs::TraversalCallback& callback, AutosarModelObject& object) {
-        dfs::traverse_model(object, callback);
+    void TreePrinter::print(model::IAutosarModel& root) {
+        generic_print(*m_callback, root);
     }
 
-    void TreePrinter::print(model::IAutosarModel& root) {
+    void TreePrinter::print(model::IModelEntry& root) {
         generic_print(*m_callback, root);
     }
 
@@ -179,6 +183,185 @@ namespace arxml::printer {
     }
 
     void TreePrinter::print(model::IAutosarElement& element) {
+        generic_print(*m_callback, element);
+    }
+
+    class ArxmlPrinterCallback : public dfs::TraversalCallback {
+    public:
+        ArxmlPrinterCallback(std::ostream& os, int tab_size)
+                : m_os{os}
+                , m_tab_size{tab_size}
+                , m_indent_level{0}
+        {}
+
+        ~ArxmlPrinterCallback() = default;
+
+        void visit(model::IAutosarModel &root) override;
+        void visit(model::IModelEntry &root) override;
+        void visit(model::IAutosarPackages &packages) override;
+        void visit(model::IAutosarPackage &package) override;
+        void visit(model::IAutosarElements &elements) override;
+        void visit(model::IAutosarElement &element) override;
+
+        void close(model::IModelEntry &root) override;
+        void close(model::IAutosarPackages &packages) override;
+        void close(model::IAutosarPackage &package) override;
+        void close(model::IAutosarElements &elements) override;
+        void close(model::IAutosarElement &element) override;
+    private:
+        void visit(model::ISimpleAutosarElement& element);
+        void visit(model::ICompositeAutosarElement& element);
+        void visit(model::INamedAutosarElement& element);
+
+        std::ostream& m_os;
+        int m_tab_size;
+        int m_indent_level;
+    };
+
+    void ArxmlPrinterCallback::visit(model::IAutosarModel& root) {
+
+    }
+
+    void ArxmlPrinterCallback::visit(model::IModelEntry& model) {
+        m_os << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+             << "<AUTOSAR xmlns=\"" << model.getXmlns() << "\" xmlns:xsi=\"" << model.getXmlnsXsi()
+             << "\" xsi:schemaLocation=\"" << model.getSchemaLocation() << "\">\n";
+        m_indent_level += 1;
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "<AR-PACKAGES>\n";
+        m_indent_level += 1;
+    }
+
+    void ArxmlPrinterCallback::visit(model::IAutosarPackages &packages) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "<AR-PACKAGES>\n";
+        m_indent_level += 1;
+    }
+
+    void ArxmlPrinterCallback::visit(model::IAutosarPackage &package) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "<AR-PACKAGE>\n";
+        m_indent_level += 1;
+    }
+
+    void ArxmlPrinterCallback::visit(model::IAutosarElements &elements) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "<ELEMENTS>\n";
+        m_indent_level += 1;
+    }
+
+    void ArxmlPrinterCallback::visit(model::IAutosarElement &element) {
+        switch (element.getType()) {
+            case model::EntryType::NAMED_ELEMENT:
+                visit(dynamic_cast<model::INamedAutosarElement &>(element));
+                break;
+            case model::EntryType::COMPOSITE_ELEMENT:
+                visit(dynamic_cast<model::ICompositeAutosarElement &>(element));
+                break;
+            case model::EntryType::STRING_ELEMENT:
+            case model::EntryType::INTEGER_ELEMENT:
+            case model::EntryType::FLOATING_ELEMENT:
+                visit(dynamic_cast<model::ISimpleAutosarElement &>(element));
+                break;
+            default:
+                break; // INVALID BRANCH
+        }
+    }
+
+    void ArxmlPrinterCallback::visit(model::ISimpleAutosarElement &element) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "<" << element.getTag();
+        const auto attributes = element.getAttributes();
+        if (not attributes.empty()) {
+            for (const auto& [name, value]: attributes) {
+                m_os << " " << name << "=\"" << value;
+            }
+            m_os << "\"";
+        }
+        m_os << ">" << std::setw(0);
+        switch (element.getType()) {
+            case model::EntryType::INTEGER_ELEMENT: {
+                m_os << dynamic_cast<model::INumberAutosarElement &>(element).getInteger();
+                break;
+            }
+            case model::EntryType::FLOATING_ELEMENT: {
+                m_os << dynamic_cast<model::INumberAutosarElement &>(element).getFloating();
+                break;
+            }
+            case model::EntryType::STRING_ELEMENT: {
+                m_os << dynamic_cast<model::IStringAutosarElement &>(element).getText();
+                break;
+            }
+            default:
+                assert(1 == 0); // invalid branch
+        }
+    }
+
+    void ArxmlPrinterCallback::visit(model::ICompositeAutosarElement &element) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "<" << element.getTag() << ">\n";
+        m_indent_level += 1;
+    }
+
+    void ArxmlPrinterCallback::visit(model::INamedAutosarElement &element) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "<" << element.getTag() << ">\n";
+        m_indent_level += 1;
+        m_os << std::setw(m_indent_level * m_tab_size) << " " <<  "<SHORT-NAME>" << element.getName() << "</SHORT-NAME>\n";
+    }
+
+    void ArxmlPrinterCallback::close(model::IModelEntry& root) {
+        m_indent_level -= 1;
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "</AR-PACKAGES>\n";
+        m_indent_level -= 1;
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "</AUTOSAR>\n";
+    }
+
+    void ArxmlPrinterCallback::close(model::IAutosarPackages &packages) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "</AR-PACKAGES>\n";
+        m_indent_level -= 1;
+    }
+
+    void ArxmlPrinterCallback::close(model::IAutosarPackage &package) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "<AR-PACKAGE>\n";
+        m_indent_level -= 1;
+    }
+
+    void ArxmlPrinterCallback::close(model::IAutosarElements &elements) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "<ELEMENTS>\n";
+        m_indent_level -= 1;
+    }
+
+    void ArxmlPrinterCallback::close(model::IAutosarElement &element) {
+        m_os << std::setw(m_indent_level * m_tab_size) << " " << "</" << element.getTag() << ">\n";
+        if (element.isComposite()) {
+            m_indent_level -= 1;
+        }
+    }
+
+    ArxmlPrinter::ArxmlPrinter(std::ostream& stream, int indent)
+            : m_callback{new ArxmlPrinterCallback{stream, indent}}
+    {
+
+    }
+
+    void ArxmlPrinter::print(model::IAutosarModel& root) {
+        for (auto& [name, entry]: root.getModelUnits()) {
+            std::cout << "entry: " << name << std::endl;
+            generic_print(*m_callback, *entry);
+        }
+    }
+
+    void ArxmlPrinter::print(model::IModelEntry& root) {
+        generic_print(*m_callback, root);
+    }
+
+    void ArxmlPrinter::print(model::IAutosarPackages& packages) {
+        generic_print(*m_callback, packages);
+    }
+
+    void ArxmlPrinter::print(model::IAutosarPackage& package) {
+        generic_print(*m_callback, package);
+    }
+
+    void ArxmlPrinter::print(model::IAutosarElements& elements) {
+        generic_print(*m_callback, elements);
+    }
+
+    void ArxmlPrinter::print(model::IAutosarElement& element) {
         generic_print(*m_callback, element);
     }
 
